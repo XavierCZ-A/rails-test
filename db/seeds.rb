7fa6f1983dca33886536bf44d3c0485f7692dad1
Xavier Caballero Zanabria <xavierCZ>
2024-02-29 13:22:07 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+StoreSection.create(name: 'carne')
+StoreSection.create(name: 'lácteos')
+StoreSection.create(name: 'panadería')
+StoreSection.create(name: 'alimentos')
+StoreSection.create(name: 'farmacia')
